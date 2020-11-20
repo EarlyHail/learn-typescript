ecd71be6f18969f90afb17d70971fd342b8071ef
EarlyHail <hojin5633@gmail.com>
2020-11-20 20:24:16 +0900

    [section2] 섹션2 완료
    타입스크립트 기본 세팅

---
+++ getting-started/index.ts
@@ -0,0 +1,12 @@
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+sum(10, 20);
+
+class Student {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
